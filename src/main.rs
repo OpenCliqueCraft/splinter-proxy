28a677c890ce1e036268a96a5f8caadfb7701e1e
regenerativep <regeneralp@gmail.com>
2021-05-11 19:44:08 -0400

    Packet mapping

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,24 @@
 #[macro_use]
+extern crate lazy_static;
+#[macro_use]
 extern crate log;
 extern crate simplelog;
 
 use std::{
+    self,
+    collections::HashMap,
+    iter::FromIterator,
     net::{
         TcpListener,
         ToSocketAddrs,
     },
     path::Path,
     sync::{
+        mpsc::{
+            self,
+            Receiver,
+            Sender,
+        },
         Arc,
         RwLock,
     },
@@ -24,7 +34,11 @@ use craftio_rs::{
 };
 use mcproto_rs::{
     protocol::{
+        HasPacketId,
+        Id,
         PacketDirection,
+        PacketErr,
+        RawPacket,
         State,
     },
     v1_16_3::{
@@ -32,6 +46,8 @@ use mcproto_rs::{
         HandshakeSpec,
         LoginStartSpec,
         Packet753 as PacketLatest,
+        Packet753Kind as PacketLatestKind,
+        PlayBlockChangeSpec,
         RawPacket753 as RawPacketLatest,
         StatusPongSpec,
         StatusResponseSpec,
@@ -59,6 +75,19 @@ use crate::config::{
     ConfigSaveError,
     SplinterProxyConfiguration,
 };
+
+mod mapping;
+use crate::mapping::{
+    process_raw_packet,
+    MapAction,
+    PacketMap,
+};
+
+enum EitherPacket {
+    Normal(PacketLatest),
+    // Raw(RawPacketLatest<'a>),
+    Raw(Id, Vec<u8>),
+}
 
 fn get_config(config_path: &str) -> Arc<SplinterProxyConfiguration> {
     let config = match SplinterProxyConfiguration::load(Path::new(config_path)) {
@@ -112,8 +141,42 @@ fn main() {
     .expect("Logger failed to initialize");
     info!("Starting Splinter proxy");
 
+    let packet_map: Arc<RwLock<PacketMap>> = Arc::new(RwLock::new(HashMap::new()));
+    {
+        let mut map = packet_map.write().unwrap();
+        map.insert(
+            PacketLatestKind::PlayBlockChange,
+            Arc::new(|raw_packet: RawPacketLatest| {
+                let packet = match raw_packet.deserialize() {
+                    Ok(packet) => packet,
+                    Err(e) => {
+                        error!("Failed to deserialize packet: {}", e);
+                        return MapAction::None;
+                    }
+                };
+                if let PacketLatest::PlayBlockChange(mut data) = packet {
+                    data.block_id = 5.into();
+                    MapAction::Client(PacketLatest::PlayBlockChange(data))
+                } else {
+                    MapAction::Client(packet)
+                }
+            }),
+        );
+    }
     let config = get_config("./config.ron");
-    let listener = TcpListener::bind(&config.bind_address).unwrap();
+    listen_for_clients(config, packet_map);
+}
+
+fn listen_for_clients(config: Arc<SplinterProxyConfiguration>, packet_map: Arc<RwLock<PacketMap>>) {
+    let listener = match TcpListener::bind(&config.bind_address) {
+        Err(e) => {
+            return error!(
+                "Failed to bind TCP listener to {}: {}",
+                &config.bind_address, e
+            )
+        }
+        Ok(listener) => listener,
+    };
     for stream in listener.incoming() {
         let stream = match stream {
             Ok(stream) => stream,
@@ -143,47 +206,45 @@ fn main() {
         };
 
         info!("Got connection from {}", sock_addr);
-
-        await_handshake(conn);
+        let packet_map = packet_map.clone();
+        thread::spawn(move || await_handshake(conn, packet_map));
     }
 }
 
-fn await_handshake(mut conn: SplinterClientConnection) {
-    thread::spawn(
-        move || match conn.craft_conn.read_raw_packet::<RawPacketLatest>() {
-            Ok(Some(RawPacketLatest::Handshake(handshake_body))) => {
-                match handshake_body.deserialize() {
-                    Ok(handshake) => {
-                        debug!(
-                            "received handshake from {}: ver {}, server {}:{}, next: {:?}",
-                            conn.sock_addr,
-                            handshake.version,
-                            handshake.server_address,
-                            handshake.server_port,
-                            handshake.next_state
-                        );
-                        match handshake.next_state {
-                            HandshakeNextState::Status => handle_status(conn),
-                            HandshakeNextState::Login => handle_login(conn),
-                        }
-                    }
-                    Err(e) => {
-                        error!(
-                            "Error parsing handshake packet from {}: {}",
-                            conn.sock_addr, e
-                        )
+fn await_handshake(mut conn: SplinterClientConnection, packet_map: Arc<RwLock<PacketMap>>) {
+    match conn.craft_conn.read_raw_packet::<RawPacketLatest>() {
+        Ok(Some(RawPacketLatest::Handshake(handshake_body))) => {
+            match handshake_body.deserialize() {
+                Ok(handshake) => {
+                    debug!(
+                        "received handshake from {}: ver {}, server {}:{}, next: {:?}",
+                        conn.sock_addr,
+                        handshake.version,
+                        handshake.server_address,
+                        handshake.server_port,
+                        handshake.next_state
+                    );
+                    match handshake.next_state {
+                        HandshakeNextState::Status => handle_status(conn),
+                        HandshakeNextState::Login => handle_login(conn, packet_map),
                     }
                 }
+                Err(e) => {
+                    error!(
+                        "Error parsing handshake packet from {}: {}",
+                        conn.sock_addr, e
+                    )
+                }
             }
-            Ok(Some(other)) => {
-                error!("Unexpected packet from {}: {:?}", conn.sock_addr, other)
-            }
-            Ok(None) => info!("Connection with {} closed before handshake", conn.sock_addr),
-            Err(e) => {
-                error!("Error reading packet from {}: {}", conn.sock_addr, e)
-            }
-        },
-    );
+        }
+        Ok(Some(other)) => {
+            error!("Unexpected packet from {}: {:?}", conn.sock_addr, other)
+        }
+        Ok(None) => info!("Connection with {} closed before handshake", conn.sock_addr),
+        Err(e) => {
+            error!("Error reading packet from {}: {}", conn.sock_addr, e)
+        }
+    }
 }
 
 fn handle_status(mut conn: SplinterClientConnection) {
@@ -217,7 +278,7 @@ fn handle_status(mut conn: SplinterClientConnection) {
     }
 }
 
-fn handle_login(mut client: SplinterClientConnection) {
+fn handle_login(mut client: SplinterClientConnection, packet_map: Arc<RwLock<PacketMap>>) {
     client.craft_conn.set_state(State::Login);
     let logindata;
     loop {
@@ -323,21 +384,45 @@ fn handle_login(mut client: SplinterClientConnection) {
 
     let (mut server_reader, mut server_writer) = server.craft_conn.into_split(); // proxy's connection to the server
     let (mut client_reader, mut client_writer) = client.craft_conn.into_split(); // proxy's connection to the client
+    let (server_writer_sender, server_writer_receiver) = mpsc::channel::<EitherPacket>();
+    let (client_writer_sender, client_writer_receiver) = mpsc::channel::<EitherPacket>();
     let is_alive_arc = Arc::new(RwLock::new(true));
+    // client reader
     {
+        let packet_map = packet_map.clone();
         let is_alive_arc = is_alive_arc.clone();
         let name = name.clone();
+        let server_writer_sender = server_writer_sender.clone();
+        let client_writer_sender = client_writer_sender.clone();
         thread::spawn(move || {
-            // pass data along from client to server
             while *is_alive_arc.read().unwrap() {
                 match client_reader.read_raw_packet::<RawPacketLatest>() {
-                    Ok(Some(raw_packet)) => match server_writer.write_raw_packet(raw_packet) {
-                        Ok(()) => {} /* trace!("Relaying raw packet client to server for {}", name), */
-                        Err(e) => error!(
-                            "Failed to relay packet client to server for {}: {}",
-                            name, e
-                        ),
-                    },
+                    Ok(Some(raw_packet)) => {
+                        match process_raw_packet(&packet_map.read().unwrap(), raw_packet) {
+                            MapAction::Relay(raw_packet) => {
+                                match server_writer_sender.send(EitherPacket::Raw(
+                                    raw_packet.id(),
+                                    raw_packet.data().to_owned(),
+                                )) {
+                                    Ok(()) => {}
+                                    Err(_e) => break,
+                                }
+                            }
+                            MapAction::Server(packet) => {
+                                match server_writer_sender.send(EitherPacket::Normal(packet)) {
+                                    Ok(()) => {}
+                                    Err(_e) => break,
+                                }
+                            }
+                            MapAction::Client(packet) => {
+                                match client_writer_sender.send(EitherPacket::Normal(packet)) {
+                                    Ok(()) => {}
+                                    Err(_e) => break,
+                                }
+                            }
+                            MapAction::None => {}
+                        }
+                    }
                     Ok(None) => {
                         info!("Client connection closed for {}", name);
                         break;
@@ -346,35 +431,111 @@ fn handle_login(mut client: SplinterClientConnection) {
                         // not sure if we should be doing something here
                         error!("Failed to read packet client to server for {}: {}", name, e);
                     }
-                };
+                }
             }
             *is_alive_arc.write().unwrap() = false;
             trace!("client to server thread closed for {}", name);
         });
     }
-
-    // pass data along from server to client
-    while *is_alive_arc.read().unwrap() {
-        match server_reader.read_raw_packet::<RawPacketLatest>() {
-            Ok(Some(raw_packet)) => {
-                match client_writer.write_raw_packet(raw_packet) {
-                    Ok(()) => {} // trace!("Relaying raw packet server to clientfor {}", name),
-                    Err(e) => error!(
-                        "Failed to relay packet server to client for {}: {}",
-                        name, e
-                    ),
+    // server writer
+    {
+        let is_alive_arc = is_alive_arc.clone();
+        let name = name.clone();
+        thread::spawn(move || {
+            let mut recv = server_writer_receiver.iter();
+            while *is_alive_arc.read().unwrap() {
+                match recv.next() {
+                    Some(packet) => match match packet {
+                        EitherPacket::Normal(packet) => server_writer.write_packet(packet),
+                        EitherPacket::Raw(id, data) => {
+                            match RawPacketLatest::create(id, data.as_slice()) {
+                                Ok(packet) => server_writer.write_raw_packet(packet),
+                                Err(_e) => continue,
+                            }
+                        }
+                    } {
+                        Ok(()) => {}
+                        Err(e) => error!("Failed to relay packet to server for {}: {}", name, e),
+                    },
+                    None => break,
                 }
             }
-            Ok(None) => {
-                info!("Server connection closed for {}", name);
-                break;
-            }
-            Err(e) => {
-                // not sure if we should be doing something here
-                error!("Failed to read packet server to client for {}: {}", name, e);
-            }
-        };
+            *is_alive_arc.write().unwrap() = false;
+        });
     }
-    *is_alive_arc.write().unwrap() = false;
-    trace!("server to client thread closed for {}", name);
+
+    // server reader
+    {
+        let packet_map = packet_map.clone();
+        let is_alive_arc = is_alive_arc.clone();
+        let name = name.clone();
+        let server_writer_sender = server_writer_sender.clone();
+        let client_writer_sender = client_writer_sender.clone();
+        thread::spawn(move || {
+            while *is_alive_arc.read().unwrap() {
+                match server_reader.read_raw_packet::<RawPacketLatest>() {
+                    Ok(Some(raw_packet)) => {
+                        match process_raw_packet(&packet_map.read().unwrap(), raw_packet) {
+                            MapAction::Relay(raw_packet) => {
+                                match client_writer_sender.send(EitherPacket::Raw(
+                                    raw_packet.id(),
+                                    raw_packet.data().to_owned(),
+                                )) {
+                                    Ok(()) => {}
+                                    Err(_e) => break,
+                                }
+                            }
+                            MapAction::Server(packet) => {
+                                match server_writer_sender.send(EitherPacket::Normal(packet)) {
+                                    Ok(()) => {}
+                                    Err(_e) => break,
+                                }
+                            }
+                            MapAction::Client(packet) => {
+                                match client_writer_sender.send(EitherPacket::Normal(packet)) {
+                                    Ok(()) => {}
+                                    Err(_e) => break,
+                                }
+                            }
+                            MapAction::None => {}
+                        }
+                    }
+                    Ok(None) => {
+                        info!("Server connection closed for {}", name);
+                        break;
+                    }
+                    Err(e) => {
+                        // not sure if we should be doing something here
+                        error!("Failed to read packet server to client for {}: {}", name, e);
+                    }
+                };
+            }
+            *is_alive_arc.write().unwrap() = false;
+            trace!("server to client thread closed for {}", name);
+        });
+    }
+    // client writer
+    let is_alive_arc = is_alive_arc.clone();
+    let name = name.clone();
+    thread::spawn(move || {
+        let mut recv = client_writer_receiver.iter();
+        while *is_alive_arc.read().unwrap() {
+            match recv.next() {
+                Some(packet) => match match packet {
+                    EitherPacket::Normal(packet) => client_writer.write_packet(packet),
+                    EitherPacket::Raw(id, data) => {
+                        match RawPacketLatest::create(id, data.as_slice()) {
+                            Ok(packet) => client_writer.write_raw_packet(packet),
+                            Err(_e) => continue,
+                        }
+                    }
+                } {
+                    Ok(()) => {}
+                    Err(e) => error!("Failed to relay packet to client for {}: {}", name, e),
+                },
+                None => break,
+            }
+        }
+        *is_alive_arc.write().unwrap() = false;
+    });
 }
