92420ac6c754da0a1ad842548d0f7c22768a8eea
William Gardner <william@gardna.net>
2021-05-18 15:36:24 -0700

    Removed unintentional chat changes

--- src/chat.rs
+++ src/chat.rs
@@ -30,45 +30,47 @@ pub fn init(map: &mut PacketMap) {
                 Ok(packet) => {
                     if let PacketLatest::PlayClientChatMessage(data) = packet {
                         info!("{}", data.message);
-                        if let Err(e) = client.servers.read().unwrap()[0] // TODO: select the correct server
-                            .writer
-                            .lock()
-                            .unwrap()
-                            .write_packet(PacketLatest::PlayClientChatMessage(
-                                PlayClientChatMessageSpec {
-                                    message: data.message,
-                                },
-                            ))
-                        {
-                            error!("Failed to send command message from {}: {}", client.name, e);
+                        match data.message.get(..1) {
+                            Some("/") => {
+                                if let Err(e) = client.servers.read().unwrap()[0] // TODO: select the correct server
+                                    .writer
+                                    .lock()
+                                    .unwrap()
+                                    .write_packet(PacketLatest::PlayClientChatMessage(
+                                        PlayClientChatMessageSpec {
+                                            message: data.message,
+                                        },
+                                    ))
+                                {
+                                    error!(
+                                        "Failed to send command message from {}: {}",
+                                        client.name, e
+                                    );
+                                }
+                            }
+                            _ => {
+                                let message = format!("{}: {}", client.name, data.message);
+                                for (id, target) in state.players.read().unwrap().iter() {
+                                    if let Err(e) = target.writer.lock().unwrap().write_packet(
+                                        PacketLatest::PlayServerChatMessage(
+                                            PlayServerChatMessageSpec {
+                                                message: Chat::Text(TextComponent {
+                                                    text: message.clone(),
+                                                    base: BaseComponent::default(),
+                                                }),
+                                                position: ChatPosition::ChatBox,
+                                                sender: client.uuid,
+                                            },
+                                        ),
+                                    ) {
+                                        error!(
+                                            "Failed to send chat message from {} to {}",
+                                            client.name, target.name
+                                        );
+                                    }
+                                }
+                            }
                         }
-
-                        // match data.message.get(..1) {
-                        //     Some("/") => {
-                        //     }
-                        //     _ => {
-                        //         let message = format!("<{}> {}", client.name, data.message);
-                        //         for (id, target) in state.players.read().unwrap().iter() {
-                        //             if let Err(e) = target.writer.lock().unwrap().write_packet(
-                        //                 PacketLatest::PlayServerChatMessage(
-                        //                     PlayServerChatMessageSpec {
-                        //                         message: Chat::Text(TextComponent {
-                        //                             text: message.clone(),
-                        //                             base: BaseComponent::default(),
-                        //                         }),
-                        //                         position: ChatPosition::ChatBox,
-                        //                         sender: client.uuid,
-                        //                     },
-                        //                 ),
-                        //             ) {
-                        //                 error!(
-                        //                     "Failed to send chat message from {} to {}",
-                        //                     client.name, target.name
-                        //                 );
-                        //             }
-                        //         }
-                        //     }
-                        // }
                     }
                 }
 
