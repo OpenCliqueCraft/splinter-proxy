9c990c6cda6d6e0143ddf6b3722fdb980536fb98
regenerativep <regeneralp@gmail.com>
2021-06-03 20:21:08 -0400

    Server setup script

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+#![allow(unused_imports)]
+#[macro_use]
+extern crate lazy_static;
+#[macro_use]
+extern crate log;
+extern crate simplelog;
+
+pub mod chat;
+pub mod config;
+pub mod connection;
+pub mod mapping;
+pub mod proto;
+pub mod state;
+pub mod zoning;
